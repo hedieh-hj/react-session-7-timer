e6daa908f9690818369cf8265398211cb842d468
hedieh-hj <hediyejamili79@gmail.com>
2023-05-29 11:01:09 +1000

    timer - session 7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const elem = (
+//   <div>
+//     <h1>time is :</h1>
+//     <h2>it is {new Date().toLocaleTimeString()}</h2>
+//   </div>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const timer = () => {
+  const elem = (
+    <div>
+      <h1>time is :</h1>
+      <h2>it is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(elem, document.getElementById("root"));
+};
+
+setInterval(() => {
+  timer();
+}, 1000);
